f00cf51c0506da9dfa977676941e0ea4b530eeee
hamaco <hamanaka.kazuhiro@gmail.com>
2014-06-03 18:03:41 +0900

    gitのコミットメッセージは自動保存しない

--- inits/40-auto-save-buffers-enhanced.el
+++ inits/40-auto-save-buffers-enhanced.el
@@ -1,4 +1,5 @@
 ; (require 'auto-save-buffers-enhanced)
 
 (setq auto-save-buffers-enhanced-interval 0.5)
+(setq auto-save-buffers-enhanced-exclude-regexps '("COMMIT_EDITMSG"))
 (auto-save-buffers-enhanced t)
