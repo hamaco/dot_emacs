5e14900a45d4204a1c7d82d2be10ab1df2299a1a
hamaco <hamanaka.kazuhiro@gmail.com>
2014-06-03 17:53:50 +0900

    git-gutterの見た目設定

--- inits/10-git.el
+++ inits/10-git.el
@@ -4,3 +4,11 @@
 
 ;; git-gutter
 (global-git-gutter-mode +1)
+(custom-set-variables
+  '(git-gutter:modified-sign "  ")  ;; two space
+  '(git-gutter:added-sign    "++")  ;; multiple character is OK
+  '(git-gutter:deleted-sign  "--"))
+
+(set-face-background 'git-gutter:modified "purple")
+(set-face-foreground 'git-gutter:added    "green")
+(set-face-foreground 'git-gutter:deleted  "red")
