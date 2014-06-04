9bbbc4ba64eeb44cf2403fd38678ac12a31ca765
hamaco <hamanaka.kazuhiro@gmail.com>
2014-06-04 11:42:42 +0900

    AquaSKKのskk-server使う

--- inits/15-skk.el
+++ inits/15-skk.el
@@ -4,9 +4,16 @@
 (require 'skk-autoloads)
 (global-set-key (kbd "C-x C-j") 'skk-mode)
 
+; for AquaSKK
+(setq skk-server-portnum 1178)
+(setq skk-server-host "localhost")
+
+
 (setq skk-show-inline t)
 (setq skk-sticky-key ";")
 (setq skk-tut-file "~/.emacs.d/etc/skk/SKK.tut")
+
+
 (let ((ws window-system))
   (cond
     ;;((eq ws 'w32) (setq skk-large-jisyo ""))
