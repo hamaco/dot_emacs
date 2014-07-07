b5718270f725bf13df32bbe5a58047bb525f20e8
hamaco <hamanaka.kazuhiro@gmail.com>
2014-07-07 10:46:00 +0900

    moe-themeアップデートしたら設定変わったのか動かなくなってた?

--- inits/99-theme.el
+++ inits/99-theme.el
@@ -12,5 +12,5 @@
 ;                     (set-face-foreground 'magit-diff-add "#b9ca4a")
 ;                     (set-face-foreground 'magit-diff-del "#d54e53")))
 
-(load-theme 'moe-dark t t)
-(enable-theme 'moe-dark)
+(require 'moe-theme)
+(moe-dark)
