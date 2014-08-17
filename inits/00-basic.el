c79c0d153c8cba578f0868b73b682b53adc4947a
hamaco <hamanaka.kazuhiro@gmail.com>
2014-08-17 14:43:59 +0900

    日本語の設定これで良いのかな?

--- inits/00-basic.el
+++ inits/00-basic.el
@@ -2,7 +2,15 @@
 
 (set-language-environment       'Japanese)
 (prefer-coding-system           'utf-8)
-(setq inhibit-startup-message t)
+;(set-buffer-file-coding-system  'utf-8)
+;(set-default-coding-systems     'utf-8)
+;(set-keyboard-coding-system     'utf-8)
+;(set-terminal-coding-system     'utf-8)
+;(set-selection-coding-system    'utf-8)
+;(setq file-name-coding-system   'utf-8)
+;(setq buffer-file-coding-system 'utf-8)
+
+(setq inhibit-startup-message t) ;; スタートアップメッセージを表示しない
 (menu-bar-mode 0)
 (tool-bar-mode 0)
 
