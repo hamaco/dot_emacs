24245ab917085e3939f73a62db01ca65dad2645e
hamaco <hamanaka.kazuhiro@gmail.com>
2014-11-04 18:25:33 +0900

    - blankline

--- init.el
+++ init.el
@@ -8,18 +8,14 @@
           (concat user-emacs-directory "vendor/skk")
           ) load-path))
 
-
 ;; cask
 (require 'cask "/usr/local/share/emacs/site-lisp/cask.el")
 (cask-initialize)
-
-
 
 ;; init-loader.el
 (defvar init-loader-show-log-after-init nil)
 (custom-set-variables '(init-loader-show-log-after-init 'error-only))
 (init-loader-load (concat user-emacs-directory "inits"))
-
 
 ;; debug
 (global-set-key (kbd "<f12>") 'eval-buffer)
